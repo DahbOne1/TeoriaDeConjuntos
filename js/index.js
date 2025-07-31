413d49dd92339826a738fa84b5892c07ab49b24c
David Hernandez <David.arcos1000@gmail.com>
2025-07-31 10:02:20 -0600

    final version

--- js/index.js
+++ js/index.js
@@ -124,3 +124,10 @@ function calcularOperaciones(){
   document.getElementById("diferenciaSim").value = difSim;
 
 }
+
+ const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('mobile-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
